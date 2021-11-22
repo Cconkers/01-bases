a2890c24fc2d5efbe40c965aaa3b6e53829b1512
Cconkers <rubenpv011992@gmail.com>
2021-11-22 18:00:25 +0100

    Lección 61

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ContadorModule } from './contadores/contador.module';
 
+import { ContadorModule } from './contadores/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesModule, ContadorModule],
+  imports: [BrowserModule, HeroesModule, ContadorModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
