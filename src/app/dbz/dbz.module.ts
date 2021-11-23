f07a6959702cef60140cfb64dcae9ff4bc66ddd3
Cconkers <rubenpv011992@gmail.com>
2021-11-23 11:54:37 +0100

    Last updated, DBZ ended

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,19 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { AgregarComponent } from './agregar/agregar.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 
-
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
-  declarations: [
-    MainPageComponent,
-    PersonajesComponent
-  ],
+  declarations: [MainPageComponent, AgregarComponent, PersonajesComponent],
   exports: [MainPageComponent],
-  imports: [
-    CommonModule,
-    FormsModule
-  ]
+  imports: [CommonModule, FormsModule],
+  providers:[DbzService]
 })
-export class DbzModule { }
+export class DbzModule {}
